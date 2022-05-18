9041a4d9118b7cedddca1a7c687920840c95591f
Warreldie <vanelverdinghe.ward@gmail.com>
2022-05-18 11:38:26 +0200

    Build: Blocks

--- resources/views/components/info.blade.php
+++ resources/views/components/info.blade.php
@@ -1,3 +1,11 @@
-<div class="border fancy max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8 w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-    <h1>Hellow</h1>
+<div class="flex flex-auto m-8 border fancy max-w-7xl py-4 px-4 sm:px-6 lg:px-8 rounded-md">
+    <div class="flex-auto bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+        <svg class="h-10 w-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd" />
+        </svg>
+    </div>
+    <div class="flex-auto">
+        <p>{{ $text }}</p>
+        <h1>1 WEEK</h1>
+    </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,10 @@
     <div class="w-full">
         <x-page-header page="Dashboard"/>
         <div class="bg-slate-200">
-            <x-info text="Box"/>
+            <div class="flex p-2">
+                <x-info text="Reveal next event"/>
+                <x-info text="Total books read"/>
+            </div>
             <x-button cta="Continue Reading"/>
         </div>
     </div>
