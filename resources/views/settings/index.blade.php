11a33a030c1709f2446168bdfe8528b1a81e8717
Warreldie <vanelverdinghe.ward@gmail.com>
2022-05-19 20:59:43 +0200

    No idea

--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -11,6 +11,11 @@
         <div class="flex">
             <div>
                 <x-side-setting heading="Profile" link1="Personal information" link2="Email settings" link3="Password settings" />
+                <x-side-setting heading="Subscription" link1="Subscription plan" link2="History" link3="Payments" />
+                <x-side-setting heading="Other" link1="Privacy & Security" link2="FAQ" link3="Contact" />
+            </div>
+            <div>
+                <x-setting />
             </div>
         </div>
     </div>
