474572e9a12c0dfddb87b5da4e9d2eafaf2d4ae4
Warreldie <vanelverdinghe.ward@gmail.com>
2022-05-19 14:45:43 +0200

    Make: first component

---
+++ resources/views/components/side-setting.blade.php
@@ -0,0 +1,6 @@
+<div class="mt-8 ml-2 px-6 py-5 content-center border rounded">
+    <h1>{{ $heading }}</h1>
+    <a href="#">{{ $link1 }}</a>
+    <a href="#">{{ $link2 }}</a>
+    <a href="#">{{ $link3 }}</a>
+</div>
--- resources/views/settings/index.blade.php
+++ resources/views/settings/index.blade.php
@@ -8,6 +8,11 @@
     </div>
     <div class="w-full">
         <x-page-header page="Settings"/>
+        <div class="flex">
+            <div>
+                <x-side-setting heading="Profile" link1="Personal information" link2="Email settings" link3="Password settings" />
+            </div>
+        </div>
     </div>
 </div>
 
