aa9d879449b6374945110d8e6235e6c26ccfebca
Warreldie <vanelverdinghe.ward@gmail.com>
2022-05-24 11:24:22 +0200

    Fix: merge conflict

--- resources/views/bookshelf/index.blade.php
+++ resources/views/bookshelf/index.blade.php
@@ -3,96 +3,6 @@
 @section('content')
 
 <div class="flex">
-<<<<<<< HEAD
-    <div class="top-0 left-0">
-        <x-navigation/>
-    </div>
-    <div>
-        <x-page-header class="w-full" page="Bookshelf"/>
-
-<div class="max-w-7xl mx-auto py-10 px-4 sm:px-6 lg:px-8">
-<div>    
-    <livewire:book-search /> 
-</div>
-<div class="absolute overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 table-auto">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Cover
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Title
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Author
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Rating
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Shelf
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Date read
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    <span class="sr-only"></span>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-    @foreach($books as $book)
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                    <img style="height: 150px" src="{{ $book->image }}"> 
-                </th>
-                <td class="px-6 py-4">
-                    The Count of Monte Cristo
-                </td>
-                <td class="px-6 py-4">
-                    <a href="/books/{{ $book->id }}">{{ $book->title }}</a>
-                </td>
-                <td class="px-6 py-4">
-                <ul class="flex justify-center">
-                    <li>
-                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                        <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                        </svg>
-                    </li>
-                    <li>
-                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                        <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                        </svg>
-                    </li>
-                    <li>
-                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                        <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                        </svg>
-                    </li>
-                    <li>
-                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500 mr-1" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                        <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                        </svg>
-                    </li>
-                    <li>
-                        <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="star" class="w-4 text-yellow-500" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                        <path fill="currentColor" d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"></path>
-                        </svg>
-                    </li>
-                </ul>
-                </td>
-                <td class="px-6 py-4">
-                    <!--<form name="statusShelf" action="test.php" method="post">
-                        <select name="status" id="status">
-                            <option value="finished">Finished</option>
-                            <option value="wanttoread">Want to read</option>
-                            <option value="reading">Reading</option>
-                        </select>
-                        <br><br>
-                    </form>  -->
-                        <!-- Dropdown menu -->
-=======
     <div class="top-0 left-0">  
         <x-navigation/>
     </div>
@@ -180,7 +90,6 @@
                             <br><br>
                         </form>  -->
                             <!-- Dropdown menu -->
->>>>>>> d277ee24829f3ba5af5c797b96a0b24fd755e713
                         <div id="dropdown" class="z-10 hidden bg-black divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
                             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefault">
                             <li>
